eb64450b05d82946ce1b11f9f56147f54993b6e8
MarekSlenker <marek.slenker@savba.sk>
2019-12-15 21:56:25 +0100

    skuska roxygen2

---
+++ R/read.morphodata.R
@@ -0,0 +1,15 @@
+#' A read.morphodata Function
+#'
+#' Imports data from tab-delimited text file and produces a "morphodata" object from it.
+#' @usage read.morphodata(infile, dec=".")
+#' @param infile the name of the file which the data are to be read from.
+#' @param dec the character used in the file for decimal points.
+#' @return Abject of class "morphodata"
+#' @keywords
+#' @export
+#' @examples
+#' read.morphodata("infile.txt")
+#' read.morphodata("clipboard")
+read.morphodata<-function(infile, dec="."){
+
+}
