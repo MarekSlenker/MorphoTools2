1b5adaadf40c63468af0f80df34d232a4151f2da
MarekSlenker <marek.slenker@savba.sk>
2020-05-29 22:22:19 +0200

    Shapiro-Wilk normality test

---
+++ R/shapirowilk.R
@@ -0,0 +1,46 @@
+#' Shapiro-Wilk Normality Test
+#' @export
+shapiroWilk.test <- function(object, p.value = 0.05) {
+  checkClass(object, "morphodata")
+
+
+  characters = colnames(object$data)
+  groups =  levels(unlist(object$Taxon))
+
+  shapiroWilkStatistic = array(numeric(), c(length(characters), length(groups)), dimnames = list(characters, groups))
+
+  shapiroWilkStatistic = as.data.frame(shapiroWilkStatistic)
+
+  # calculate descr statistics
+  for (group in groups) {
+    groupPositions = which( unlist(object$Taxon) %in% group)
+
+    shapiroWilkStatistic[group] = apply(object$data[groupPositions, ], 2,
+                                        function(x) {
+                                          out <- tryCatch(
+                                            {
+                                              shapiro.test(x)$p.value
+                                            },
+                                            error=function(cond) {
+                                              return(NaN)
+                                            }
+                                          )
+                                          return(out)
+                                        }
+                                      )
+  }
+
+  shapiroWilkStatistic = as.matrix(shapiroWilkStatistic)
+
+  if (! is.na(p.value)) {
+
+    greaterThenP = which(shapiroWilkStatistic > p.value)
+    lowerThenP = which(shapiroWilkStatistic <= p.value)
+
+    shapiroWilkStatistic[greaterThenP] = "normally distributed"
+    shapiroWilkStatistic[lowerThenP] = "NOT normally distributed"
+
+  }
+  return(shapiroWilkStatistic)
+}
+
