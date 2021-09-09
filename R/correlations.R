0ea90f4bf787940b86ab90b08265a039e2163129
MarekSlenker <marek.slenker@savba.sk>
2021-09-09 23:46:19 +0200

    formatujem tuto

--- R/correlations.R
+++ R/correlations.R
@@ -1,11 +1,11 @@
 #' Correlations of characters
 #' @export
-cormat <- function (object, method = "pearson") {
+cormat <- function (object, method = "Pearson") {
   .checkClass(object, "morphodata")
 
-  if (! (method %in% c("pearson", "spearman"))) stop(paste("Method \"", method , "\" is not supported.", sep = ""), call. = FALSE)
+  if (! (method %in% c("Pearson", "Spearman", "pearson", "spearman"))) stop(paste("Method \"", method , "\" is not supported.", sep = ""), call. = FALSE)
 
-  corelations = stats::cor(object$data, use="pairwise.complete.obs", method = method)
+  corelations = stats::cor(object$data, use="pairwise.complete.obs", method = tolower(method))
   corelations = round(corelations, digits = 3)
   corelations = data.frame(corelations)
 
@@ -19,9 +19,9 @@ cormat <- function (object, method = "pearson") {
 
 #' @rdname cormat
 #' @export
-cormatSignifTest <- function(object, method = "pearson", alternative = "two.sided") {
+cormatSignifTest <- function(object, method = "Pearson", alternative = "two.sided") {
 
-  if (! (method %in% c("pearson", "spearman"))) stop(paste("Method \"", method , "\" is not supported.", sep = ""), call. = FALSE)
+  if (! (method %in% c("Pearson", "Spearman", "pearson", "spearman"))) stop(paste("Method \"", method , "\" is not supported.", sep = ""), call. = FALSE)
 
   table = matrix(data = numeric(), nrow = ncol(object$data), ncol = ncol(object$data))
   colnames(table) = colnames(object$data)
@@ -30,7 +30,7 @@ cormatSignifTest <- function(object, method = "pearson", alternative = "two.side
   for (row in colnames(object$data)) {
     for (col in colnames(object$data)) {
 
-      cc = stats::cor.test(x = object$data[, row], y = object$data[, col], method = method,
+      cc = stats::cor.test(x = object$data[, row], y = object$data[, col], method = tolower(method),
                     use="pairwise.complete.obs", alternative = alternative)
 
       table[row, col] = paste( round(cc$estimate, digits = 3), ";p-value=", round(cc$p.value, digits = 5), sep = "")
